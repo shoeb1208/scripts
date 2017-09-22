af00e7d18d75edd01f050202163ab35ff711e0c4
shoeb1208 <shoeb1208@gmail.com>
2017-09-22 11:17:06 +0800

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
-
+import sys
 import json
 import csv
 from pprint import pprint
-with open('test.json') as data_file:
-    data = json.load(data_file)
+
+try:
+        file=sys.argv[1]
+except:
+        print "Usage:",sys.argv[0],"json_file_path"
+        sys.exit(1)
+
+with open(file) as data_file:
+        data = json.load(data_file)
 print "ID,impactScore,baseScore,exploitabilityScore"
 for item in data['CVE_Items']:
         print item["cve"]["CVE_data_meta"]["ID"],",",item["impact"]["baseMetricV2"]["impactScore"],",",item["impact"]["baseMetricV2"]["cvssV2"]["baseScore"],",",item["impact"]["baseMetricV2"]["exploitabilityScore"]
